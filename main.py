5e6f10b6633da50fb1c4c8ade5ebb4d5ac0cf8b5
Bruno Baialuna <brunobaialuna@hotmail.com>
2022-07-14 21:24:10 -0300

    Incluído input que solicita o nome do usuário

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # App de Cálculo de IMC
 
+nome = input("Digite seu nome: ")
 peso = float(input("Digite seu peso: "))
 altura = float(input("Digite sua altura: "))
 
 imc = peso / altura ** 2
 
-print(f'Seu imc é {imc:.2f}')
+print(f'{nome}, seu imc é {imc:.2f}')
 
